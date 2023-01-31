13817096098c4f274df15383f1e417819b20ca48
Yuliya_K <live2do@yandex.ru>
2023-01-31 22:15:44 +0300

    add Задача 8. Чётные от 1 до N

---
+++ 8/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает
+// число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int numberN = int.Parse(Console.ReadLine());
+if (numberN < 0) numberN = -1 * numberN;
+int index = 1;
+
+while (index <= numberN)
+{
+    if (index % 2 == 0)
+    {
+        Console.Write($"{index}, ");
+    }    
+    index++;
+}
