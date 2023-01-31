f31806ad12982ea1b57a2545aa9e69b0cac7a6c7
Yuliya_K <live2do@yandex.ru>
2023-01-31 21:49:34 +0300

    add Задача 6. Четное или нечетное

---
+++ 6/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает 
+// число и выдает, является ли число четным (делится ли оно на 
+// два без остатка).
+
+Console.Clear();
+Console.Write("Введите целое число: ");
+int numb = int.Parse(Console.ReadLine());
+
+if (numb % 2 == 0)
+{
+    Console.WriteLine("ДА");
+    Console.WriteLine($"Число {numb} - четное");
+}
+else
+{
+    Console.WriteLine("НЕТ");
+    Console.WriteLine($"Число {numb} - нечетное");
+}
+
+
